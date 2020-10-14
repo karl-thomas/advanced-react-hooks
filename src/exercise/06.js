a1c3409684dd44ea0daac61ca1003b922c8adb93
karl-thomas <karl.thomas925@gmail.com>
2020-10-14 03:38:46 -0500

    label our custom hook so that it's easy to find in dev tools

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -3,11 +3,11 @@
 
 import React from 'react'
 
+const formatMediaDebugValue = ({query, state}) => `\`${query}\` => ${state}`
+
 function useMedia(query, initialState = false) {
   const [state, setState] = React.useState(initialState)
-  // 🐨 call React.useDebugValue here.
-  // 💰 here's the formatted label I use: `\`${query}\` => ${state}`
-
+  React.useDebugValue({query, state}, formatMediaDebugValue)
   React.useEffect(() => {
     let mounted = true
     const mql = window.matchMedia(query)
